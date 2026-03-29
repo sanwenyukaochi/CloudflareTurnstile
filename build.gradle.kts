51d2248101d6ec624b2a7fe67faceac719d3ca90
sanwenyukaochi <sanwenyukaochi@outlook.com>
2026-03-29 22:56:53 +0800

    update: dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java-library")
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.diffplug.spotless") version "8.3.0"
+    id("com.diffplug.spotless") version "8.4.0"
 }
 
 group = "io.github.sanwenyukaochi"
@@ -13,28 +13,28 @@ java {
     }
 }
 
-val springBootVersion by extra("4.0.3")
-val lombokVersion by extra("1.18.42")
+extra["springBootVersion"] = "4.0.5"
+extra["lombokVersion"] = "1.18.42"
 
 dependencies {
     // Spring Boot dependencies
-    compileOnly("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
-    compileOnly("org.springframework.boot:spring-boot-starter-actuator:$springBootVersion")
+    compileOnly("org.springframework.boot:spring-boot-starter-web:${property("springBootVersion")}")
+    compileOnly("org.springframework.boot:spring-boot-starter-actuator:${property("springBootVersion")}")
 
     // Lombok dependencies
-    compileOnly("org.projectlombok:lombok:$lombokVersion")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:$springBootVersion")
-    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+    compileOnly("org.projectlombok:lombok:${property("lombokVersion")}")
+    annotationProcessor("org.projectlombok:lombok:${property("lombokVersion")}")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:${property("springBootVersion")}")
 
     // Lombok dependencies for test classes
-    testCompileOnly("org.projectlombok:lombok:$lombokVersion")
-    testAnnotationProcessor("org.projectlombok:lombok:$lombokVersion")
-    testAnnotationProcessor("org.springframework.boot:spring-boot-configuration-processor:$springBootVersion")
+    testCompileOnly("org.projectlombok:lombok:${property("lombokVersion")}")
+    testAnnotationProcessor("org.projectlombok:lombok:${property("lombokVersion")}")
+    testAnnotationProcessor("org.springframework.boot:spring-boot-configuration-processor:${property("springBootVersion")}")
 
     // Testing dependencies
-    testImplementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
-    testImplementation("org.springframework.boot:spring-boot-starter-actuator:$springBootVersion")
+    testImplementation("org.springframework.boot:spring-boot-starter-web:${property("springBootVersion")}")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:${property("springBootVersion")}")
+    testImplementation("org.springframework.boot:spring-boot-starter-actuator:${property("springBootVersion")}")
     testImplementation("org.junit.jupiter:junit-jupiter:6.0.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
